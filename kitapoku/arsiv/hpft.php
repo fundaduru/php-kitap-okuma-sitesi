d4d1ce93f2c65dd973e5315237187d0095256651
Fundaduru <63059388+fundaduru@users.noreply.github.com>
2023-09-20 13:53:16 +0300

    Update hpft.php

--- kitapoku/arsiv/hpft.php
+++ kitapoku/arsiv/hpft.php
@@ -11,20 +11,6 @@
             <i>
 
 
-Küçük yaşta ailesini bir trafik kazasında kaybeden Harry Potter, teyzesi, eniştesi ve kuzeni Dudley ile birlikte yaşamaktadır. Eniştesi, teyzesi ve kuzeni Harry’e çok kötü davranmaktadırlar ve Harry merdiven boşluğunun altındaki bir odada yaşamaktadır. Çok mutsuz bir çocukluk geçiren Harry’nin 11 inci doğum günü yaklaşmaktadır.<br><br>
-
-Bir gün Harry’e bir mektup gelir fakat eniştesi mektubu okumasına izin vermez. Fakat mektupların arkası kesilmez hatta giderek artan sayıda mektuplar gelmeye devam eder. Bir gün evin her yerinden mektup yağmaya başlar. Eniştesi evdekileri de alarak evden ayrılırlar ve çok uzak bir yere giderler. Denizin ortasındaki bir adada, küçük bir kulübede yaşamaya başlarlar. Bir akşam kulübeye Hagrid adında dev gibi bir adam gelir. Hagrid, Hogwarts Cadılık ve Büyücülük Okulu’nun bekçisidir. Hagrid, Harry’nin mektupları okuyamadığını öğrenir ve her şeyi anlatır. Anne ve babası trafik kazasında ölmemişlerdir. Onlar büyücüdürler ve kötü büyücü Voldemort tarafından öldürülmüşlerdir. Harry de bir büyücü adayıdır. Harry, bugüne kadar tüm gerçekleri kendisinden saklayan teyzesi ve eniştesine çok kızar. Hogwarts Büyücülük Okuluna gitmeye karar verir. Okul öncesi alışverişi için Hagrid ile birlikte Diagon Yolu’na giderler. Buradan bir asa, uçan süpürge, büyücülük kitapları, haberci bir baykuş gibi okulda ihtiyaç olan şeyleri alırlar.<br><br>
-
-Okulun başlayacağı 1 Eylül günü Londra Paddington Tren İstasyonu’na gider. Okula gidecek Hogwarts Ekspresi’nin bulunduğu 9 ¼ peronundan trene biner. Harry trende Ron ve Hermione adında iki arkadaş edinir.<br><br>
-
-Hogwart’da okula yeni başlayan çocukların kalacakları binalar ve sınıflar seçim ile belirlenmektedir. Öğrenciler büyük bir şapkayı giyerler ve şapka onlara hangi sınıfta olacaklarını söyler. Okulda dört bina ve sınıf vardır. Gryffindor, Hufflepuff, Ravenclaw ve Slytherin. Slytherin’den genellikle kötü büyücüler çıkmaktadır. Harry’nin okul süresince hep rakibi olacak Malfoy Slytherin’ne seçilir. Harry, Ron ve Hermione ise aynı sınıfa, Gryffindor’a seçilirler. Artık dersler başlamıştır. Harry ve arkadaşları değişik alanlarda büyücülük dersleri almaya başlarlar. Çok farklı maceralar onları beklemektedir artık.<br><br>
-
-Bir gün Harry ve arkadaşları okulda girilmesi yasak olan üçündü koridora giderler. Burada üç başlı bir canavarın koruduğu “Felsefe Taşı” bulunmaktadır. Felsefe Taşı sahibine ölümsüzlük ve üstün güçler vermektedir. Harry’nin anne ve babasını öldüren kötü büyücü Voldemort da Felsefe Taşının peşindedir. Harry taşı bulur ve Voldemort ile ilk mücadelesini yaşar. Harry Voldemort’u etkisiz hale getirir.<br><br>
-
-Bu arada okulun sınıfları arasında çeşitli yarışmalar düzenlenmektedir. Gryffindor Harry’nin üstün başarıları ile birinci olur. Artık okul bitmiş ve yaz tatili gelmiştir. Öğrenciler Hogwart Ekspresi ile yaz tatili için evlerine giderler. Harry bu durumdan pek hoşnut değildir, çünkü tatilini geçirebileceği bir ailesi yoktur ve şimdiden okulunu özlemiştir.
-
-
-
 
 
 
