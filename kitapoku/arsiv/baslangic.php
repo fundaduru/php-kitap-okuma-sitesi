2035ac159895d930ac65b4e0ded8591e4c112636
Fundaduru <63059388+fundaduru@users.noreply.github.com>
2023-09-20 13:54:17 +0300

    Update baslangic.php

--- kitapoku/arsiv/baslangic.php
+++ kitapoku/arsiv/baslangic.php
@@ -11,19 +11,6 @@
             <i>
 
 
-Kim olursan ol, neye inanırsan inan,
-Çok yakında her şey değişecek...<br><br>
-
-Genç adam, aniden üç büyük dinin temsilcilerine döndü. “Şaşırtıcı bulacağınızı tahmin ettiğim bilimsel bir buluşum sebebiyle bugün buradayım. İnsanlık deneyimimizin en temel iki sorusuna cevap bulma ümidi ile yıllardır peşinden koşuyordum. Bu bilginin tüm inananları derinden etkileyeceğine inanıyorum. Nasıl desem, ‘yıkıcı’ diye tanımlanabilecek bir değişikliğe sebep olabilir. Birazdan görecekleriniz, dünyayla paylaşmayı umduğum sunumun kaba bir kesiti. Fakat bunu yapmadan önce dünyanın en etkili din adamlarına danışmak, en çok etkilenecek kişilerce nasıl algılanacağını öğrenmek istedim.”<br><br>
-
-Piskopos, haham ve ulema birbirlerine baktılar, sıkılmış görünüyorlardı. Piskopos, “İlginç bir girizgâh Bay Kirsch. Bize gösterecekleriniz dünya dinlerinin temelini sarsacakmış gibi konuşuyorsunuz," dedi. Genç adam kutsal metinlerin saklandığı bu eski mahzende etrafına baktı. Temellerini sarsmayacak, yıkacak, diye düşündü. Din adamları üç gün içinde bu sunumu bir etkinlikle insanlara duyuracağını bilmiyorlardı. Bunu yaptığında tüm insanlar, dini öğretilerin gerçekten de ortak bir noktası bulunduğunu anlayacaklardı: Hepsinin tümden yanlış olduğunu...<br><br>
-
-Nereden geldik? Nereye gidiyoruz?<br><br>
-
-İnsanoğlunun var olduğu günden beri cevabını bulmaya çalıştığı bu temel soruya cevap bulma iddiasındaki bir fütüristin tam da keşfini açıklayacağı gece her şey trajik bir biçimde karanlığa gömülür. Eski öğrencisinin sunumuna davetli olan Simgebilim Profesörü Robert Langdon söz konusu keşfi öğrencisinin anısına dünyaya duyurmaya karar verir. Ancak, kendisini bekleyen şifrelerden, acı sürprizlerden ve ölümcül fanatiklerden habersizdir...
-
-
-
 
 
 
