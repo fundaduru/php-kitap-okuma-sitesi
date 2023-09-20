6ca07414ef9b3e0459ace01f391d467646209336
Fundaduru <63059388+fundaduru@users.noreply.github.com>
2023-09-20 13:05:21 +0300

    Update küçükprens.php

--- kitapoku/arsiv/küçükprens.php
+++ kitapoku/arsiv/küçükprens.php
@@ -10,24 +10,6 @@
             <i>
 
 
-Gün doğarken uykusunun arasında garip, incecik bir ses duyar. Karşısında ilginç, minik biri durmaktadır. Bu, Küçük Prens’tir. Yani yazar Küçük Prens adında birisiyle gelmiştir. Gezegeninde tek başına yaşamaktadır. Biri sönmüş ikisi hala lavlar püskürten üç tane yanardağa vardır. Ayrıca hiçbir gezegende bulunmayan eşsiz güzellikte bir tek de çiçeği vardır. Küçük Prens pilotumuza “Bana bir koyun çizer misiniz?” diye bir soru yöneltir. Pilot Küçük Prens’in bu sorusuna cevap vermek için uyanmıştır. Etrafına bakınır. Şaşkındır. Ama gördüğü gerçektir. Rüya değildir. Pilotumuz büyük bir şaşkınlık içerisinde “iyi resim yapmayı beceremem” der. Bu yeteneğini büyüklerin küçükken söylediği sözler yüzünden geliştiremediğini söyler.<br><br>
-
-Küçük Prens “önemli değil” der. Aynı soruyu tekrar eder. Yazar altı yaşındayken çizdiği boğa yılanını çizer. Bu resmi altı yaşındayken büyüklerine de göstermiştir. Onlar hiç beğenmediklerini, resmin bir şapkaya benzediğini söylemişlerdir. Bunun üzerine pilot resim yapma isteğini kaybetmiştir. Çünkü büyüklerin hepsi resim çizmek yerine tarih, coğrafya, matematik ve dilbilgisiyle ilgilenmesini öğütlemişlerdir.<br><br>
-
-Küçük Prens ise pilotun kendisine çizdiği resme bakar ve “Ben boğa yılanı içinde bir fil çizmeni istemiyorum. Bana bir koyun çizer misin?” diye sorusunu tekrar eder. Yazar Küçük Prens’in çizdiği resmi anlamasından dolayı şaşkındır, işte şimdi bir koyun çizmeye karar verir. Küçük Prens çizilen resmi beğenmez. Pilot bu sefer bir kutu çizer ve koyunun kutunun içinde olduğunu söyler. Şimdi olmuştur Küçük Prens bu resmi beğenir.<br><br>
-
-
-Bundan sonra aralarındaki iletişim artar. Birbirleri hakkında bilgi edinirler. Pilot bu farklı dünyadan gelin küçük adamın sırrını çözmeye, onu anlamaya çalışır. Pilot çizdiği koyun resmi ile ilgili Küçük Prens’in ona sorduğu sorulardan Küçük Prens’i daha iyi tanır. Yanı Küçük Prens’in gezegeninin küçük olduğu, üç yanardağının ve bir çiçeğinin olduğunu öğrenir. (“Koyunlar kaçar mı?, Koyunlar çiçekleri yerler mi?”).<br><br>
-
-Küçük Prens yaşadığı yerden bahseder. Yaptığı gezileri anlatır. Bu gezileri, değişik gezegenlerde yaşayan insanlar ve bu insanların meslekleri, ilgi alanları, huylan ile ilgili edindiği izlenimleriyle birlikte pilotumuza anlatır.<br><br>
-
-Örneğin bir gezegende kırmızı suratlı bir adam olduğunu ve onun hiç çiçek koklatmadığını anlatır. Gezegen iri birinde her şeyi yönettiğini söyleyen kral, bir diğerinde kendini beğenmiş bir adam, bir başka gezegende unutmak için içtiğini söyleyen bir adam, sayılarla uğraşan işadamı ve buluşlarını kaydeden bir coğrafyacıyla da tanıştığı ve pilota anlattığı kişilerdir.<br><br>
-
-Küçük Prens bundan bir yıl önce dünyaya gezmek için geldiğini zamanda şimdiki bulundukları yerde olduğunu söyler. Diğer gezegenlere! olan şeylerden dünyada binlerce olduğunu görür.<br><br>
-
-Yazar Küçük Prens’in anılarını, yaşam hakkındaki düşüncelerini dinler. Ayrılık vakti gelir. Yazarın evine, Küçük Prens’in geldiği gezegene dönme zamanıdır.
-
-Pilot yaşadığı bu güzel anıyı kimseye anlatmaz. Üzerinden altı yıl geçtikten sonra küçük dostunu unutmamak için kaleme almaya karar verir.
 
 
 
