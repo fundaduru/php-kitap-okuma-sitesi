7880cde5af3d70d9424595444b874bc9269af983
Fundaduru <63059388+fundaduru@users.noreply.github.com>
2023-09-20 13:52:51 +0300

    Update hpso.php

--- kitapoku/arsiv/hpso.php
+++ kitapoku/arsiv/hpso.php
@@ -10,25 +10,6 @@
 
             <i>
 
-Harry Potter yaz tatilini üvey ailesi Dursley’lerle birlikte geçirmiş, bu tatil onun için işkenceye dönmüş ve bir an önce okula dönmek istemektedir. Bu sıralarda Harry’nin odasında Dobby adında ev cini belirmiş ve Harry’e eğer okula dönerse başına kötü şeyler geleceğini, ölebileceğini söyler. Dobby Harry’i korumak için onun okula gitmesini engellemeye çalışır ama başarılı olamaz. Harry Potter okula geldiğinde esrarengiz olaylarla karşılaşmaya başlar. İlk olarak Harry Potter çatal dilinde sesler duyar. Ses Harry’i kendisine doğru çağırır. Daha sonra Harry’i koridorda yürürken sesi tekrar duyar.<br><br>
-
-Ses Harry’e “Kan kokusu alıyorum, öldüreceğim öldüreceğim” der. Ardından Harry’i koşmaya başlar ve Filch’in kedisini taşa döndüğünü ve duvarda “Sırlar odası açıldı” yazısını görür. Bu yazının üzerine düşen Harry Ron ve Hermione sırlar odasının gizemini araştırmaya başlar. 1000 yıl öncesinde okulu kuran 4 kişiden biri olan Salazar Slytherin’in okula alımlarda daha seçici olunması gerektiğini söylüyor sadece saf kan büyücü olan kişilerin okula alınması gerektiğini söylüyordu. Ama diğerlerini ikna edemeyince okuldan ayrıldı. Okulda gizli bir yer inşa ediyor ve varisi gelene kadar kapıyı mühürlüyor. Sadece varis kapısını açabilecek içindeki dehşeti ortaya çıkartacak ve saf kan olmayan büyücüleri temizleyecektir.<br><br>
-
-Okulda taşa dönüşen kişi sayısı artınca bakanlık Dumbledore’u görevden alır ve Hagrit’i sırlar odasını açmakla suçlayıp Azkaban’a gönderirler. Harry’i kızlar tuvaletinde bir günlük bulur. Günlüğün üstünde Tom Marvolo Riddle yazmaktadır. Bu günlükte hiçbir şey yazmaz ama bu günlük Harry Potter ile konuşur. Harry Potter sırlar odası hakkında ne bildiğini günlüğe sorar ama yanıt alamaz. Günlük onu 50 sene önceye götürür. 50 sene önce sırlar odası tekrar açılmış ve bir muggle aileye sahip büyücünün ölmesiyle sonuçlanmıştır.<br><br>
-
-Harry’nin gittiği anıda Tom Riddle adında bir büyücü Dumbladore ile konuşur. Dumbladore Tom’a eğer suçlu bulunmazsa okulun kapanacağını söyler. Bunun ardından Tom suçu Hagrit’in örümceğine atar ve konu böyle kapanır. İşler artık kontrolden çıkmıştır. Harry’nin yakın arkadaşı Hermione de taşa dönüşmüştür. Hermione hastanede yattığı zaman yanına Harry Potter gelir ve Hermione’yi görür. Hermione‘nin elinde de kütüphane bir kitaptan yırttığı sayfa vardır. Kağıtta basilisk yılanı hakkında bilgi vardır. Bu bilgi yılanın gözüne bakan kişinin öldüğü şeklindedir. Ama kimse yılanın gözüne doğrudan bakmadığı için ölmez taşa dönüşür. Bu ipucundan sonra canavar Ginny Weasley’i kaçırır.<br><br>
-
-Ginny Weasley kaçırılınca Dumladore görevinin başına tekrar geçer. Harry ve Ron sırlar odasına girişin kızlar tuvaletindeki çeşmede olduğunu öğrenir. Harry Potter çeşmeye çatal dilinde konuşur ve gizli geçit açılır. Buradan içeri doğru girerler. İçeri girdiklerinde tavan çöker ve Harry’i artık yalnız ilerlemek zorunda kalır. Yılanlı kapıdan geçtikten sonra Ginny Weasley’i görür.<br><br>
-
-Ginny yerde hareketsiz yatmaktadır. Harry hemen yanına doğru gider. Bu sırada içeri Tom Rİddle anı şeklinde girer. Harry Tom Riddle ile konuşmaya başlar. Tom Riddle’ın Salazar Slytherin’in varisi olduğu sırlar odasını 50 sene önce onun açtırdığını ve şimdi de Ginny’i günlüğü kullanarak kandırdığını ve sırlar odasını açtırdığını öğrenir. Hepsinden önemlisi Salazar Slytherin’in varisi Tom Riddle’ın Voldemort olduğunu öğrenir. Tom Riddle Basiliks’i Harry’i öldürmesi için yuvasından çağırır.<br><br>
-
-Harry Basiliks’den kaçarken ayağı takılır, yere düşer. Basiliks Harry’İ öldürmek için davrandığında Fawkes adlı anka kuşu gelir ve yılanın gözlerini oyar. Daha sonra Fawkes tekrar gelir. Bu sefer pençelerinde seçmen şapka vardır. Fawkes Harry’e seçmen şapkayı bırakır. Seçmen şapkanın içinden Gryffindor’un kılıcı çıkar. Bu esnada Tom Rİddle Harry’e Ginny’i öldürdükten sonra anı olmaktan çıkacağını kanlı canlı bir şekilde geri döneceğini söyler. Harry Basiliks ile savaşırken kılıcı Basiliks’e saplar.<br><br>
-
-Basiliks’in dişi Harry’nin koluna saplanmıştır. Basiliks’in zehri Harry’nin vücuduna yayılırken Harry Basiliksin dişini kolundan çıkarır ve Voldemort’un anı olarak yaşadığı Tom Rİddle’ın günlüğüne doğru batırır. Tom Riddle’ın günlüğü ve anısı yok olur. Zehir Harry’nin vücudunda dolaşırken Fawkes tekrar gelir. Göz yaşını Harry’nin koluna doğru akıtır. Harry’nin kolu iyileşir çünkü Anka Kuşlarının göz yaşları iyileştirici etkiye sahiptir. Ginny, Harry ve taşa dönüşen herkes kurtulur. Haggrit’in suçsuz olduğu anlaşılınca Azkaban’dan çıkar. Harry Dumladore ile konuşurken Dumbladore Harry’e Voldemort’un yara izini bıraktığı gün Harry’e bazı güçlerini aktardığını o yüzden Harry ile Voldemort arasında benzerlik bulunduğunu söyler.
-
-
-
-
 
 
 
