484621a6914c60382208250c427c901cb66a69a3
Fundaduru <63059388+fundaduru@users.noreply.github.com>
2023-09-20 13:05:47 +0300

    Update kürkmantolumadonna.php

--- kitapoku/arsiv/kürkmantolumadonna.php
+++ kitapoku/arsiv/kürkmantolumadonna.php
@@ -12,12 +12,7 @@
 
 
 
-KÜRK MANTOLU MADONNA ÖZET<br><br>
-Kitap, Rasim'in işinden olması ve akabinde yeni bir iş arayışı ile başlar. Bu iş arayışı içinde bir gün rastladığı eski arkadaşı Hamdi’den yardım ister. Bu istek karşılık bulur ve Rasim, biraz utansa da Hamdi’nin müdürü olduğu işyerinde çalışması için ettiği teklifi kabul eder.<br><br>
 
-Rasim yeni işindeki odasında yaşı ilerlemiş Raif Efendi adlı sessiz, sakin bir adamla çalışacaktır. Hemen hemen hiç konuşmayan, verilen işleri titizlikle yapan Raif efendi, boş vakitlerinde de çekmecesindeki kitabı okumaktadır.<br><br>
-
-Bu Raif Efendi’nin içine kapanık, melankolik hali Rasim’in merakını celbeder. Raif Efendi'nin hastalanıp işe gelmediği bir gün yapılacak bir çevirinin ona ulaştırılması gerektiğinden Rasim, Raif , şirketin Almanca çeviri işlerini yapan Raif Efendi'nin evinin yolunu tutmasıyla kafasındaki soru işaretlerine cevap bulacaktır.
 
 
 
