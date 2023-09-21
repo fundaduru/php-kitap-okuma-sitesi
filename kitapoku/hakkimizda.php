5ff4423489b5b10f316c252904970b0a24894c74
Fundaduru <fundaduru0@gmail.com>
2023-09-21 09:25:23 +0300

    Update hakkimizda.php

--- kitapoku/hakkimizda.php
+++ kitapoku/hakkimizda.php
@@ -1,7 +1,7 @@
 <?php
 include("header.php");
 ?>
-<em  style="font-size: 40px;""color:#black">HAKKIMIZDA</em><br><br>
+<em  style="font-size: 40px; color: #black">HAKKIMIZDA</em><br><br>
         <div  >
             Kitapoku sitesi 2022 yılında kitap okumayı yaygınlaştırmak ve okuyucuya ücretsiz erişim sağlamak amacıyla kurulmuştur.
 
